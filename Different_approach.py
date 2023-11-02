f0b3aba4c39b86b9e2f47255481e7f780c9aed4f
ThomasSTI <thomaslaene@gmail.com>
2023-11-02 11:02:23 +0100

    Different_approach.py

--- Different_approach.py
+++ Different_approach.py
@@ -1,52 +1,55 @@
 import requests
 import pandas as pd
 
-def fetch_and_filter_data(url, years, kommuner):
-    filtered_data = []
+def fetch_data(year):
+    url = f"https://skatteverket.entryscape.net/rowstore/dataset/c67b320b-ffee-4876-b073-dd9236cd2a99?år={year}"
+    
+    group_ett_kommuner = ["BOTKYRKA","DANDERYD","EKERÖ","HANINGE","HUDDINGE",
+                        "JÄRFÄLLA","LIDINGÖ","NACKA","NORRTÄLJE","NYKVARN",
+                        "NYNÄSHAMN","SALEM","SIGTUNA","SOLLENTUNA","SOLNA",
+                        "STOCKHOLM","SUNDBYBERG","SÖDERTÄLJE","TYRESÖ","TÄBY",
+                        "UPPLANDS VÄSBY","UPPLANDS-BRO","VALLENTUNA","VAXHOLM","VÄRMDÖ",
+                        "ÖSTERÅKER"]
 
-    for year in years:
-        year_url = f"{url}?år={year}"
+    try:
+        response = requests.get(url)
 
-        try:
-            response = requests.get(year_url)
+        if response.status_code == 200:
+            data = response.json()
+            results = data["results"]
 
-            if response.status_code == 200:
-                data = response.json()
-                results = data["results"]
+            filtered_data = []
 
-                for result in results:
-                    kommun = result["kommun"].upper()
-                    if kommun in kommuner:
-                        kommunal_skatt = result["kommunal-skatt"]
-                        excel_data = {"Year": year, "Kommun": kommun, "Kommunal Skatt": kommunal_skatt}
-                        filtered_data.append(excel_data)
-                        
-            else:
-                print(f"Failed to retrieve data for year {year}. Status code:", response.status_code)
+            for result in results:
+                kommun = result["kommun"].upper()
+                if kommun in group_ett_kommuner:
+                    kommunal_skatt = result["kommunal-skatt"]
+                    excel_data = {"Year": year, "Kommun": kommun, "Kommunal Skatt": kommunal_skatt}
+                    filtered_data.append(excel_data)
+                    
+            return filtered_data
 
-        except Exception as error:
-            print(f"An error occurred for year {year}:", str(error))
+        else:
+            print(f"Failed to retrieve data for year {year}. Status code:", response.status_code)
+            return []
 
-    return filtered_data
+    except Exception as error:
+        print(f"An error occurred for year {year}:", str(error))
+        return []
 
-if __name__ == "__main__":
-    url = "https://skatteverket.entryscape.net/rowstore/dataset/c67b320b-ffee-4876-b073-dd9236cd2a99"
-    years = list(range(2014, 2024))
-    kommuner = ["BOTKYRKA","DANDERYD","EKERÖ","HANINGE","HUDDINGE",
-                "JÄRFÄLLA","LIDINGÖ","NACKA","NORRTÄLJE","NYKVARN",
-                "NYNÄSHAMN","SALEM","SIGTUNA","SOLLENTUNA","SOLNA",
-                "STOCKHOLM","SUNDBYBERG","SÖDERTÄLJE","TYRESÖ","TÄBY",
-                "UPPLANDS VÄSBY","UPPLANDS-BRO","VALLENTUNA","VAXHOLM","VÄRMDÖ",
-                "ÖSTERÅKER"]
+years = list(range(2014, 2024))
+all_data = []
 
-    data = fetch_and_filter_data(url, years, kommuner)
+for year in years:
+    data = fetch_data(year)
+    all_data.extend(data)
 
-    if data:
-        df = pd.DataFrame(data)
-        df.drop_duplicates(subset=["Year", "Kommun"], keep="first", inplace=True)
-        df = df.pivot(index="Kommun", columns="Year", values="Kommunal Skatt")
-        df.columns = [f"{year}" for year in df.columns]
-        df.to_excel("all_kommunal_skatt.xlsx", index=False)
-        print("Data saved to all_kommunal_skatt.xlsx")
-    else:
-        print("No data to save.")
+if all_data:
+    df = pd.DataFrame(all_data)
+    df.drop_duplicates(subset=["Year", "Kommun"], keep="first", inplace=True)
+    df = df.pivot(index="Kommun", columns="Year", values="Kommunal Skatt")
+    df.columns = [f"{year}" for year in df.columns]
+    df.to_excel("all_kommunal_skatt.xlsx", index=False)
+    print("Data saved to all_kommunal_skatt.xlsx")
+else:
+    print("No data to save.")
