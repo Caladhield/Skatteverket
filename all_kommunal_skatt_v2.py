bf4f3b68107cf1d6df54fade41fed4272990830f
Dimitris <113204121+Dimitris1886@users.noreply.github.com>
2023-11-05 11:32:58 +0100

    all_kommunall_skatt_version 2
    Extra exercise

---
+++ all_kommunal_skatt_v2.py
@@ -0,0 +1,66 @@
+#%%
+import requests
+import pandas as pd
+#%%
+def fetch_data(year):
+    url = f"https://skatteverket.entryscape.net/rowstore/dataset/c67b320b-ffee-4876-b073-dd9236cd2a99?år={year}"
+    
+    group_ett_kommuner = ["BOTKYRKA","DANDERYD","EKERÖ","HANINGE","HUDDINGE",
+                        "JÄRFÄLLA","LIDINGÖ","NACKA","NORRTÄLJE","NYKVARN",
+                        "NYNÄSHAMN","SALEM","SIGTUNA","SOLLENTUNA","SOLNA",
+                        "STOCKHOLM","SUNDBYBERG","SÖDERTÄLJE","TYRESÖ","TÄBY",
+                        "UPPLANDS VÄSBY","UPPLANDS-BRO","VALLENTUNA","VAXHOLM","VÄRMDÖ",
+                        "ÖSTERÅKER"]
+
+    try:
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            data = response.json()
+            results = data["results"]
+
+            filtered_data = []
+
+            for result in results:
+                kommun = result["kommun"].upper()
+                if kommun in group_ett_kommuner:
+                    kommunal_skatt = result["kommunal-skatt"]
+                    excel_data = {"Year": year, "Kommun": kommun, "Kommunal Skatt": kommunal_skatt}
+                    filtered_data.append(excel_data)
+                    
+            return filtered_data
+
+        else:
+            print(f"Failed to retrieve data for year {year}. Status code:", response.status_code)
+            return []
+
+    except Exception as error:
+        print(f"An error occurred for year {year}:", str(error))
+        return []
+
+years = list(range(2014, 2024))
+all_data = []
+
+for year in years:
+    data = fetch_data(year)
+    all_data.extend(data)
+
+if all_data:
+    df = pd.DataFrame(all_data)
+    df.drop_duplicates(subset=["Year", "Kommun"], keep="first", inplace=True)
+    df = df.pivot(index="Kommun", columns="Year", values="Kommunal Skatt")
+    df.columns = years#
+    
+    alla_kommuner = df.index.tolist()
+    df["Kommun"] = alla_kommuner
+    
+    df = df[["Kommun"] + [col for col in df.columns if col != "Kommun"]]
+    # Calculate standard deviation-std
+    df[years] = df[years].astype(float) # convert to float, type was object. To compute the "std"
+    df['Standard_deviation'] = df[years].std(axis=1, numeric_only=True).round(3)#axis = 1  to compute std to row-wise
+
+    df.to_excel("all_kommunal_skatt.xlsx", index=False)
+    print("Data saved to all_kommunal_skatt.xlsx")
+else:
+    print("No data to save.")
+# %%
